681792e39c5c18f3f7a7d817678306d68e4acb75
Chakaitos <chakaitos@gmail.com>
2013-12-05 16:20:57 -0500

    Fix js-routes bug

--- config/application.rb
+++ config/application.rb
@@ -60,6 +60,6 @@ module Treebook
     config.assets.version = '1.0'
 
     # Setting for deployment on Heroku (devise)
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
   end
 end
