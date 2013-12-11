bceaa2b961b54cbc87488c7a50abd48449495b1b
Chakaitos <chakaitos@gmail.com>
2013-12-11 11:53:38 -0500

    Refactor functional tests

--- test/functional/statuses_controller_test.rb
+++ test/functional/statuses_controller_test.rb
@@ -57,6 +57,14 @@ class StatusesControllerTest < ActionController::TestCase
     assert_redirected_to status_path(assigns(:status))
   end
 
+  test "should create an activity item for the status when logged in" do
+    sign_in users(:chakaitos)
+
+    assert_difference('Activity.count') do
+      post :create, status: { content: @status.content }
+    end
+  end
+
   test "should create status for the current user when logged in" do
     sign_in users(:chakaitos)
 
@@ -95,6 +103,13 @@ class StatusesControllerTest < ActionController::TestCase
     sign_in users(:chakaitos)
     put :update, id: @status, status: { content: @status.content }
     assert_redirected_to status_path(assigns(:status))
+  end
+
+  test "should create an activity item when the status is updated" do
+    sign_in users(:chakaitos)
+    assert_difference 'Activity.count' do 
+      put :update, id: @status, status: { content: @status.content }
+    end
   end
 
   test "should update status for the current user when logged in" do
